7a6b89f4ca9bd55c246992cd1a404fac21c1b12d
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-11-18 10:04:41 -0200

    added the sequelize and the first model 'user'

---
+++ server/models/user.ts
@@ -0,0 +1,26 @@
+export default function (sequelize, DataTypes) {
+
+    const User = sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    })
+
+    return User;
+}
--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,16 @@ import * as http from 'http';
 import Api from './api/api';
 import Logger from './config/logger';
 import appConfig from './config/app-config';
+const models = require('./models');
 
 const server = http.createServer(Api);
 let port = appConfig().serverPort;
 
 new Logger();
 
-server.listen(port);
-server.on('listening', () => console.log(`Server is up in port ${port}`));
-server.on('error', (error: NodeJS.ErrnoException) => console.log(`Error on server, ${error}`));
-
+models.sequelize.sync().then(() => {
+    server.listen(port);
+    server.on('listening', () => console.log(`Server is up in port ${port}`));
+    server.on('error', (error: NodeJS.ErrnoException) => console.log(`Error on server, ${error}`));
+});
 
