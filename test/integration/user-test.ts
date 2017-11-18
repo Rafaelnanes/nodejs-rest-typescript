4eab0efc226ef5c155152ea1fbbf2bc4ed310774
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-11-18 18:48:47 -0200

    tests improvements

--- test/integration/user-test.ts
+++ test/integration/user-test.ts
@@ -1,15 +1,33 @@
 import * as HTTPStatus from 'http-status';
 import { app, request, assert } from './config/helpers';
+var token;
 
-describe('Testes de Integração', () => {
+describe('Integration Tests', () => {
+
+    describe('/POST login', function () {
+        var user = { "login": "admin", "password": "admin" };
+        it('Should return token', (done) => {
+            request(app)
+                .post('/login')
+                .send(user)
+                .end(function (error, res) {
+                    assert.equal(200, res.status);
+                    token = res.headers['authorization'];
+                    assert.isNotNull(token);
+                    done();
+                });
+        });
+    });
+
     describe('GET /', () => {
         it('Should return all users', done => {
             request(app)
                 .get('/user')
+                .set('authorization', token)
                 .end((error, res) => {
                     assert.isNotNull(res);
                     assert.equal(HTTPStatus.OK, res.status);
-                    assert.equal(res.body.length, 1);
+                    assert.equal(res.body.length, 2);
                     done(error);
                 })
         })
