c44dd06fa2357d3e81ea5a3cffb4d796e241f20e
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-11-19 08:56:20 -0200

    added separated context for test environment

--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -1,12 +1,12 @@
 module.exports = {
     env: 'test',
-    db: 'node_dev',
+    db: 'node_test',
     dialect: 'mysql',
     username: 'root',
     password: '1234',
     host: 'localhost',
     serverPort: 3000,
     dbPort: 3306,
-    url: 'jdbc:mysql://localhost:3306/node_dev',
+    url: 'jdbc:mysql://localhost:3306/node_test',
     secret: 'mySecret'
 }
