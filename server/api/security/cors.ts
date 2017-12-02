d05ad3677abb7fc573660ba6c138f98a9da39924
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-12-02 14:19:00 -0200

    fixing cors

--- server/api/security/cors.ts
+++ server/api/security/cors.ts
@@ -9,11 +9,11 @@ class CORS {
     public register(app: Application): void {
         app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", `${req.headers.origin}`);
-            res.header('Access-Control-Allow-Methods', 'POST, GET,OPTIONS, DELETE, PUT');
+            res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT');
             res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, Authorization");
             res.header("Access-Control-Expose-Headers", "Authorization");
             if ('OPTIONS' == req.method) {
-                return res.send(200);
+                return res.sendStatus(200);
             }
             next();
         });
